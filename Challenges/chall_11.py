e5452a5d0e0b64738a126b95f1331f3f2d5f5062
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-06-01 20:49:13 -0400

    Solve challenge 11

--- Challenges/chall_11.py
+++ Challenges/chall_11.py
@@ -4,7 +4,7 @@
 
 # Uses Anaconda environment with Pillow for image processing
 
-import PIL, PIL.Image
+from PIL import Image
 import numpy as np
 
 
@@ -13,19 +13,30 @@ def main():
     Hint: odd even
     '''
 
-
     photo_path = './cave.jpg'
-    photo = PIL.Image.open(photo_path)
-    width = photo.size[0]
-    height = photo.size[1]
+    photo = Image.open(photo_path)
+    width, height = photo.size # 640, 480
 
+    # Load image into numpy array
+    pixel_data = np.asarray(photo)
+    # pixel_data.shape: 480 rows, 640 pixels/row, 3 points with RGB info
+    even_pixels = np.zeros((height, width//2, 3), dtype=np.uint8)
+    odd_pixels = np.zeros((height, width//2, 3), dtype=np.uint8)
 
+    # Split image into two by even and odd pixels
+    for row in range(height):
+        even_pixels[row] = pixel_data[row,::2,:] if row % 2 == 0 else pixel_data[row,1::2,:]
+        odd_pixels[row] = pixel_data[row,::2,:] if row % 2 == 1 else pixel_data[row,1::2,:]
 
-    # photo.save('cave_final.jpg')
+    # Draw new images and save
+    even_photo = Image.fromarray(even_pixels, "RGB")
+    even_photo.save('even.jpg')
+    odd_photo = Image.fromarray(odd_pixels, "RGB")
+    odd_photo.save('odd.jpg')
 
     return 0
 
-# Keyword:
+# Keyword: evil
 
 
 if __name__ == '__main__':
