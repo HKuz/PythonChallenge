07385de62aec963b9516ec9dde9fd2931bdd8909
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-06-02 13:22:44 -0400

    Solve challenge 12

---
+++ Challenges/chall_12.py
@@ -0,0 +1,25 @@
+#!/Applications/anaconda/envs/Python3/bin
+# Python challenge - 12
+# http://www.pythonchallenge.com/pc/return/evil.html
+
+
+def main():
+    '''
+    Hint: evil1.jpg -> evil2.jpg not .jpg - _.gfx -> evil3.jpg No more evils
+    evil2.gfx has 5 jpgs in it
+    Can't install gfx module on MBP :(
+    '''
+
+    with open('./evils/evil2.gfx', 'rb') as evil:
+        evil = evil.read()
+
+        for i in range(5):
+            open('./evils/evil_image'+str(i)+'.jpg', 'wb').write(evil[i::5])
+
+    return 0
+
+# Keyword: disproportional
+
+
+if __name__ == '__main__':
+    main()
