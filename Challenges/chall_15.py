ddd309d504cf73379f28de3f2ca7ba8786afb5bc
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-06-04 19:47:40 -0400

    Set up challenge 15

---
+++ Challenges/chall_15.py
@@ -0,0 +1,22 @@
+#!/Applications/anaconda/envs/Python3/bin
+# Python challenge - 15
+# http://www.pythonchallenge.com/pc/return/cat.html
+# http://www.pythonchallenge.com/pc/return/uzi.html
+
+import datetime
+
+def main():
+    '''
+    Hint: whom? he ain't the youngest, he is the second
+    todo: buy flowers for tomorrow
+    January 26th is circled on a calendar
+    '''
+    
+
+    return 0
+
+# Keyword:
+
+
+if __name__ == '__main__':
+    main()
