b4a6db5b0b6d7509e0aa281f7d3fde19192ab8dd
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-06-07 13:29:29 -0400

    Solve challenge 16

--- Challenges/chall_16.py
+++ Challenges/chall_16.py
@@ -4,21 +4,41 @@
 
 # Uses Anaconda environment with Pillow for image processing
 
-from PIL import Image, ImageDraw
+from PIL import Image
+import numpy as np
 
 def main():
     '''
     Hint: let me get this straight
-    Image has bars of 4 pink pixels with two white ones on ends
+    Image has bars of 5 pink pixels with two white ones on ends, seemingly one/row
+    Need to align the pink bars
     '''
     photo_path = './mozart.gif'
     photo = Image.open(photo_path)
     width, height = photo.size # 640, 480
-    # rgb(195, 195, 195)
+    mode = photo.mode # P
+
+    # 5th row (index 4), 106th pixel (index 105) is pink
+    # print('Pink pixel: {}'.format(photo.getpixel((106, 4)))) # 195
+    pink = 195
+
+    photo_array = np.asarray(photo)
+    shifted_array = np.zeros((height, width), dtype=np.uint8)
+
+    for y in range(height):
+        for x in range(width - 4):
+            if photo.getpixel((x, y)) == pink and photo.getpixel((x+4, y)) == pink:
+                # Number of pixels from pink to end is width - x
+                shifted_array[y, 0:width-x] = photo_array[y, x:]
+                shifted_array[y, width-x:] = photo_array[y,0:x]
+
+    # Create new photo from re-aligned rows
+    new_img = Image.fromarray(shifted_array, mode)
+    new_img.save('mozart_16_solution.gif')
 
     return 0
 
-# Keyword: mozart
+# Keyword: romance
 
 
 if __name__ == '__main__':
