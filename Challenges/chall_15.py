d012c11dea7be6b07d03dfde9fcfd933ad05c3d4
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-06-07 20:57:05 -0400

    Optimized challenge 15, set up challenge 18

--- Challenges/chall_15.py
+++ Challenges/chall_15.py
@@ -3,29 +3,23 @@
 # http://www.pythonchallenge.com/pc/return/cat.html
 # http://www.pythonchallenge.com/pc/return/uzi.html
 
-from datetime import datetime, timedelta
 import calendar
 
 def main():
     '''
     Hint: whom? he ain't the youngest, he is the second
     todo: buy flowers for tomorrow
-    January 26th is circled on a calendar
-    Year is a leap year, maybe ending in 6
+    January 26th (a Monday) is circled on a calendar
+    Year shows 1XX6 and is a leap year
     '''
     valid_years = []
-    for y in range(1600, 2017, 1):
+    for y in range(1006, 1996, 10):
         # Check if leap year and January 26th was a Monday (0)
         if calendar.isleap(y) and calendar.weekday(y, 1, 26) == 0:
             # print(y)
-            valid_years.append(str(y))
+            valid_years.append(y)
 
-    ends_in_six = []
-    for year in valid_years:
-        if year[-1] == '6':
-            ends_in_six.append(year)
-
-    print(ends_in_six) # 1756, 1976
+    print(valid_years) # [1176, 1356, 1576, 1756, 1976], Not youngest so 1756
     # Mozart born on January 27, 1756
 
     return 0
--- Challenges/chall_18.py
+++ Challenges/chall_18.py
@@ -1,13 +1,16 @@
 #!/Applications/anaconda/envs/Python3/bin
 # Python challenge - 18
 # http://www.pythonchallenge.com/pc/return/balloons.html
+# http://www.pythonchallenge.com/pc/return/brightness.html
+# http://www.pythonchallenge.com/pc/return/deltas.gz
 
 
 def main():
     '''
     Hint: can you tell the difference? (side by side pics of swans, one's darker than the other)
+    brightness -> maybe consider deltas.gz
     '''
-
+    
 
     return 0
 
