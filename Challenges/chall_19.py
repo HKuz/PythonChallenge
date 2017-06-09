d0b7809439e0da0b162b402110f5d500fac7a768
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-06-09 12:26:08 -0400

    Solve challenge 19

--- Challenges/chall_19.py
+++ Challenges/chall_19.py
@@ -2,6 +2,9 @@
 # Python challenge - 19
 # http://www.pythonchallenge.com/pc/hex/bin.html
 
+import base64
+import wave
+import audioop
 
 def main():
     '''
@@ -26,11 +29,40 @@ def main():
     --===============1295515792==--
 
     -->
+
+    sorry.html (www.pythonchallenge.com/pc/hex/.html) -> -"what are you apologizing for?"
     '''
-    
+    # Decode base64 encoding, write to a wav file?
+    with open('./indian.txt', 'r') as rawtext:
+        data = rawtext.read()
+        decoded_bytes = base64.b64decode(data)
+        temp = open('./temp.wav', 'wb')
+        temp.write(decoded_bytes)
+        temp.close()
+
+    temp_wav = wave.open('./temp.wav', 'rb')
+    indian = wave.open('./indian.wav', 'wb')
+    bigindian = wave.open('./bigindian.wav', 'wb')
+
+    nchannels, sampwidth, framerate, nframes, comptype, compname = temp_wav.getparams()
+    print('Number audio channels (1 mono, 2 stereo): {}\nSample width in bits: {}\nSample freq (frame rate): {}\nNumber audio frames: {}\nCompression type (NONE is only supported type): {}\nHuman-readable compression type: {}'.format(nchannels, sampwidth, framerate, nframes, comptype, compname))
+
+    frames = temp_wav.readframes(nframes)
+    frames_swap = audioop.byteswap(frames, sampwidth)
+
+    indian.setparams((nchannels, sampwidth, framerate, nframes, comptype, compname))
+    bigindian.setparams((nchannels, sampwidth, framerate, nframes, comptype, compname))
+
+    indian.writeframes(frames) # little endian -> 'sorry'
+    bigindian.writeframes(frames_swap) # big endian -> 'you are an idiot, ah ah ah ah ah ah ah'
+
+    temp_wav.close()
+    indian.close()
+    bigindian.close()
+
     return 0
 
-# Keyword:
+# Keyword: sorry, idiot
 
 
 if __name__ == '__main__':
