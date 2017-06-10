7bc78efeb62fd81cfce5378a0f64ae73ed6960d1
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-06-10 19:27:47 -0400

    Set up challenge 20

---
+++ Challenges/chall_20.py
@@ -0,0 +1,18 @@
+#!/Applications/anaconda/envs/Python3/bin
+# Python challenge - 20
+# http://www.pythonchallenge.com/pc/hex/idiot2.html
+
+
+def main():
+    '''
+    Hint: go away! Picture is fence with sign 'Private property beyond this fence'
+    '''
+
+
+    return 0
+
+# Keyword:
+
+
+if __name__ == '__main__':
+    main()
