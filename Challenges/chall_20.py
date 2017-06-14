069bb4f67baf69e2f9b457173ee033708ff43d25
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-06-13 22:25:57 -0400

    Start challenge 20

--- Challenges/chall_20.py
+++ Challenges/chall_20.py
@@ -3,11 +3,19 @@
 # http://www.pythonchallenge.com/pc/hex/idiot2.html
 
 
+import urllib.request
+
 def main():
     '''
     Hint: go away! Picture is fence with sign 'Private property beyond this fence'
+    But inspecting it carefully is allowed
     '''
+    url = 'http://www.pythonchallenge.com/pc/hex/idiot2.html'
+    username = 'butter'
+    password = 'fly'
 
+    # with urllib.request.urlopen(url, data={'username': username, 'password': password}) as page:
+    #     print(page.into())
 
     return 0
 
