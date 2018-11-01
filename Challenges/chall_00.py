778a4784d6fad8bede6f2355c8ae0338292844fe
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-01 13:33:03 -0400

    Create challenge script for challenge 0

---
+++ Challenges/chall_00.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3
+# Python challenge - 0
+# http://www.pythonchallenge.com/pc/def/0.html
+# Keyword: 274877906944
+
+
+def main():
+    # Hint: picture of 2^38, try to change the URL address
+    print('2^38: {}'.format(2**38))
+
+
+if __name__ == '__main__':
+    main()
