6500d388fa894bb0ea8cb0ca1328a73cc54ba4e8
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-01 14:21:01 -0400

    Refactor code, add page hints

--- Challenges/chall_02.py
+++ Challenges/chall_02.py
@@ -3,15 +3,26 @@
 # http://www.pythonchallenge.com/pc/def/ocr.html
 # Keyword: equality
 
+import string
+
 
 def main():
-    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-    letters = []
+    '''
+    Hint: recognize the characters. maybe they are in the book,
+    but MAYBE they are in the page source.
+    Page source text saved in garbage.txt
+    '''
+
+    alphabet = string.ascii_letters
     with open('garbage.txt', 'r') as garbage:
-        for line in garbage.readlines():
-            for c in line:
-                if c in alphabet:
-                    letters.append(c)
+        letters = [c for line in garbage.readlines() for c in line
+                   if c in alphabet]
+        # Long form of nested loops:
+        # letters = []
+        # for line in garbage.readlines():
+        #     for c in line:
+        #         if c in alphabet:
+        #             letters.append(c)
 
     print(''.join(letters))
     return 0
