7dc08364cbe513ce4b81483d9330789f5893fcee
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-01 14:32:22 -0400

    Refactor code, add hints from page

--- Challenges/chall_03.py
+++ Challenges/chall_03.py
@@ -1,18 +1,18 @@
 #!/usr/local/bin/python3
 # Python challenge - 3
 # http://www.pythonchallenge.com/pc/def/equality.html
+# Keyword: linkedlist
 
 import re
 
 
-'''
-Hint:
-One small letter surrounded by EXACTLY three big bodyguards on each of
-its sides.
-'''
-
-
 def main():
+    '''
+    Hint:
+    One small letter surrounded by EXACTLY three big bodyguards on each of
+    its sides.
+    Page source text saved in bodyguard.txt
+    '''
     with open('bodyguard.txt', 'r') as bodyguard:
         pattern = re.compile(r'[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]')
         littles = ''
@@ -21,8 +21,6 @@ def main():
 
     print(''.join(littles))
     return 0
-
-# Keyword: linkedlist
 
 
 if __name__ == '__main__':
