ea25c8170a03d7c75229507e332b4f1cfd468e63
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-06 11:02:56 -0500

    Move xml structure to comment

--- Challenges/chall_13.py
+++ Challenges/chall_13.py
@@ -10,14 +10,42 @@ import xmlrpc.client
 
 def main():
     '''
+    Title: call him
     Hint: phone that evil
     <area shape="circle" coords="326,177,45" href="../phonebook.php">
+    xml tree:
+    <methodResponse>
+      <fault>
+        <value>
+          <struct>
+            <member>
+              <name>faultCode</name>
+              <value>
+                <int>105</int>
+              </value>
+            </member>
+            <member>
+              <name>faultString</name>
+              <value>
+                <string>
+                  XML error: Invalid document end at line 1, column 1
+                </string>
+              </value>
+            </member>
+          </struct>
+        </value>
+      </fault>
+    </methodResponse>
     '''
-    xml_string = '<methodResponse><fault><value><struct><member><name>faultCode</name><value><int>105</int></value></member><member><name>faultString</name><value><string>XML error: Invalid document end at line 1, column 1</string></value></member></struct></value></fault></methodResponse>'
     server_url = 'http://www.pythonchallenge.com/pc/phonebook.php'
 
     with xmlrpc.client.ServerProxy(server_url) as server_proxy:
         print(server_proxy.system.listMethods())  # get a 'phone'
+        # for method in server_proxy.system.listMethods():
+        #     print('Method help for {}:'.format(method))
+        #     print(server_proxy.system.methodHelp(method))
+        #     print('Method signature for {}:'.format(method))
+        #     print(server_proxy.system.methodSignature(method))
         print(server_proxy.system.methodHelp('phone'))
         print(server_proxy.system.methodSignature('phone'))
         try:
