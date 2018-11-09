19d98cfed8554fee97dbf6f3e6a7fce7adc31411
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-09 16:12:42 -0500

    Solve challenge 20

--- Challenges/chall_20.py
+++ Challenges/chall_20.py
@@ -112,16 +112,6 @@ def main():
     with open('./invader_chall_20/message.zip', 'wb') as file:
         file.write(response.read())
 
-    '''
-    Yes! This is really level 21 in here.
-    And yes, After you solve it, you'll be in level 22!
-
-    Now for the level:
-
-    * We used to play this game when we were kids
-    * When I had no idea what to do, I looked backwards.
-    '''
-
     return 0
 
 
