58c66f16b0cb90f7bd4f563f52b66fef07c7f53e
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-09 12:04:52 -0500

    Continue challenge 20

--- Challenges/chall_20.py
+++ Challenges/chall_20.py
@@ -17,23 +17,22 @@ def main():
     '''
     # url = 'http://www.pythonchallenge.com/pc/hex/idiot2.html'
     url = 'http://www.pythonchallenge.com/pc/hex/unreal.jpg'
-    auth = base64.b64encode(b'butter:fly')
+    # Looking at headers on page, see Authorization: Basic YnV0dGVyOmZseQ==
+    # Use b64 encoding to get value, decode() to change from byte to str
+    auth = base64.b64encode(b'butter:fly').decode()
     # content_type = ('Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0)'
     #                 'Gecko/20100101 Firefox/62.0')
 
-    # headers = {
-    #     'User-Agent': content_type,
-    #     'Authorization': 'Basic {}'.format(auth.decode())
-    # }
+    headers = {
+        # 'User-Agent': content_type,
+        'Authorization': 'Basic {}'.format(auth)
+    }
 
-    # with urllib.request.urlopen(url, headers=headers) as page:
-    #     print(page.info())  # HTTP Error 401: Unauthorized
-
-    request = urllib.request.Request(url)
-    request.add_header('Authorization', 'Basic {}'.format(auth.decode()))
-    print(request.headers)
+    request = urllib.request.Request(url, headers=headers)
+    # print(request.headers)
     response = urllib.request.urlopen(request)
     print(response.headers)  # Content-Range: bytes 0-30202/2123456789
+    request.add_header()
 
     return 0
 
