40d2f50a06334db39e4264bc333fece8193efb78
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-09 13:33:01 -0500

    Continue challenge 20

--- Challenges/chall_20.py
+++ Challenges/chall_20.py
@@ -2,10 +2,11 @@
 # Python Challenge - 20
 # http://www.pythonchallenge.com/pc/hex/idiot2.html
 # Username: butter; Password: fly
-# Keyword:
+# Keyword: invader, redavni
 
-import urllib.request
 import base64
+import urllib.request
+import re
 
 
 def main():
@@ -14,25 +15,19 @@ def main():
         this fence'
     But inspecting it carefully is allowed
     '''
-    # url = 'http://www.pythonchallenge.com/pc/hex/idiot2.html'
     url = 'http://www.pythonchallenge.com/pc/hex/unreal.jpg'
     # Looking at headers on page, see Authorization: Basic YnV0dGVyOmZseQ==
     # Use b64 encoding to get value, decode() to change from byte to str
     auth = base64.b64encode(b'butter:fly').decode()
-    # content_type = ('Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0)'
-    #                 'Gecko/20100101 Firefox/62.0')
-
-    headers = {
-        # 'User-Agent': content_type,
-        'Authorization': 'Basic {}'.format(auth)
-    }
+    headers = {'Authorization': 'Basic {}'.format(auth)}
 
     request = urllib.request.Request(url, headers=headers)
     # print(request.headers)
     response = urllib.request.urlopen(request)
-    # print(response.getcode())  # 200
-    print(response.headers)
+    # print(response.getcode())  # 200, type -> int
+    # print(response.headers)
     '''
+    HEADER:
     Content-Type: image/jpeg
     Content-Range: bytes 0-30202/2123456789
     Connection: close
@@ -40,9 +35,66 @@ def main():
     Date: Fri, 09 Nov 2018 16:44:12 GMT
     Server: lighttpd/1.4.35
     '''
-    request.headers['Range'] = 'bytes=30203-'
+
+    messages, content_ranges = [], []
+    regex = re.compile(r'bytes\s?(\d+-(\d+))/(\d+)')
+
+    while True:
+        try:
+            match = regex.search(response.headers['Content-Range'])
+            endbytes, total_size = match.group(2), match.group(3)
+            content_ranges.append(match.group(1))
+            request.headers['Range'] = 'bytes={}-'.format(int(endbytes) + 1)
+            response = urllib.request.urlopen(request)
+            message = response.read().decode()
+            messages.append(message)
+            # print(message, end='')
+        except urllib.error.HTTPError:
+            # Break for HTTP Error 416: Requested Range Not Satisfiable
+            break
+
+    # print(''.join(messages))
+    # print('Content Ranges:')
+    # print('\n'.join(content_ranges))
+    '''
+    Why don't you respect my privacy?
+    we can go on in this way for really long time.
+    stop this!
+    invader! invader!
+    ok, invader. you are inside now.
+
+    Content Ranges:
+    0-30202
+    30203-30236
+    30237-30283
+    30284-30294
+    30295-30312
+    30313-30346
+    '''
+
+    # Try to request bytes after size
+    request.headers['Range'] = 'bytes={}-'.format(int(total_size) + 1)
+    match = regex.search(response.headers['Content-Range'])
+    endbytes = match.group(2)
+    content_ranges.append(match.group(1))
+    request.headers['Range'] = 'bytes={}-'.format(int(endbytes) + 1)
     response = urllib.request.urlopen(request)
-    print(response.read().decode())
+    message = response.read().decode()
+    print(message)
+    print('Message reversed: {}'.format(message[::-1]))
+    print(response.headers)
+    # the password is your new nickname in reverse: redavni
+
+    '''
+    HEADER:
+    Content-Type: application/octet-stream
+    Content-Transfer-Encoding: binary
+    Content-Range: bytes 2123456744-2123456788/2123456789
+    Connection: close
+    Transfer-Encoding: chunked
+    Date: Fri, 09 Nov 2018 18:26:03 GMT
+    Server: lighttpd/1.4.35
+    '''
 
     return 0
 
