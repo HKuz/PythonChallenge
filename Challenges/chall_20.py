ef1ad5f9956441d95e8527dda6f2c93e2f0ef060
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-08 21:58:56 -0500

    Start challenge 20

--- Challenges/chall_20.py
+++ Challenges/chall_20.py
@@ -5,6 +5,8 @@
 # Keyword:
 
 import urllib.request
+import base64
+import urllib.parse
 
 
 def main():
@@ -13,16 +15,31 @@ def main():
         this fence'
     But inspecting it carefully is allowed
     '''
-    url = 'http://www.pythonchallenge.com/pc/hex/idiot2.html'
-    username = 'butter'
-    password = 'fly'
+    # url = 'http://www.pythonchallenge.com/pc/hex/idiot2.html'
+    url = 'http://www.pythonchallenge.com/pc/hex/unreal.jpg'
+    auth = base64.b64encode(b'butter:fly')
+    # content_type = ('Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0)'
+    #                 'Gecko/20100101 Firefox/62.0')
 
-    with urllib.request.urlopen(
-            url, data={'username': username, 'password': password}) as page:
-        print(page.into())
+    # headers = {
+    #     'User-Agent': content_type,
+    #     'Authorization': 'Basic {}'.format(auth.decode())
+    # }
+
+    # with urllib.request.urlopen(url, headers=headers) as page:
+    #     print(page.info())  # HTTP Error 401: Unauthorized
+
+    request = urllib.request.Request(url)
+    request.add_header('Authorization', 'Basic {}'.format(auth.decode()))
+    print(request.headers)
+    response = urllib.request.urlopen(request)
+    print(response.headers)  # Content-Range: bytes 0-30202/2123456789
 
     return 0
 
 
 if __name__ == '__main__':
     main()
+
+
+# headers={'username': username, 'password': password}
