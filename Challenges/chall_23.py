bc9c453f1e0179f7dd1c2c7a5aa1238f34d6927c
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-09 21:22:34 -0500

    Set up challenge 23

---
+++ Challenges/chall_23.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python3
+# Python Challenge - 23
+# http://www.pythonchallenge.com/pc/hex/bonus.html
+# Username: butter; Password: fly
+# Keyword:
+
+import urllib.request
+import urllib.parse
+
+
+def main():
+    '''
+    Hint: what is this module? Picture of the cow/bull
+    TODO: do you owe someone an apology? now it is a good time to
+        tell him that you are sorry. Please show good manners although
+        it has nothing to do with this level.
+    it can't find it. this is an undocumented module.
+    '''
+    # From level 17
+    violin_url = 'http://www.pythonchallenge.com/pc/stuff/violin.php'
+    message = 'sorry'
+    quoted = urllib.parse.quote_plus(message)
+    request = urllib.request.Request(
+        violin_url, headers={'Cookie': 'info=' + quoted}
+    )
+    response = urllib.request.urlopen(request).read().decode()
+    print(response)
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
