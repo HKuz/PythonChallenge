728ca2dfe9ed8567e3f0e146fc2fc23808af13f3
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-11 13:10:35 -0500

    Save maze.png to challenge folder

--- Challenges/chall_24.py
+++ Challenges/chall_24.py
@@ -1,29 +1,25 @@
-#!/usr/local/bin/python3
+#!/Applications/anaconda/envs/imgPIL/bin
 # Python Challenge - 24
 # http://www.pythonchallenge.com/pc/hex/ambiguity.html
 # Username: butter; Password: fly
 # Keyword:
 
-import urllib.request
-import urllib.parse
-import codecs
+'''
+Uses Anaconda environment with Pillow for image processing
+    - Python 3.7, numpy, and Pillow (PIL)
+    - Run `source activate imgPIL`, `python chall_24.py`
+'''
+
+from PIL import Image
 
 
 def main():
     '''
-
+    From top to bottom, picture is a huge maze
     '''
-    # From level 17
-    violin_url = 'http://www.pythonchallenge.com/pc/stuff/violin.php'
-    message = codecs.decode('sorry', 'rot-13')
-    print(message)
-    # sorry in German: 'Es tut uns leid'
-    quoted = urllib.parse.quote_plus(message)
-    request = urllib.request.Request(
-        violin_url, headers={'Cookie': 'info=' + quoted}
-    )
-    response = urllib.request.urlopen(request).read().decode()
-    print(response)  # didn't work
+    img_path = './maze_chall_24/maze.png'
+    with Image.open(img_path) as maze:
+        print(maze.histogram())
 
     return 0
 
