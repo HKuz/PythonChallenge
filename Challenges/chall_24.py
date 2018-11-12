961d183073ce482cbb886b00eb14783697479ce2
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-11 20:49:23 -0500

    Get start and end to maze

--- Challenges/chall_24.py
+++ Challenges/chall_24.py
@@ -16,11 +16,23 @@ from PIL import Image
 def main():
     '''
     From top to bottom; (picture is a huge maze with different colored
-        pixels in the maze walls)
+        pixels in the maze walls - black pixels in 1st and last rows
+        indicate start and end points)
+    Shortest path to solve maze
     '''
     img_path = './maze_chall_24/maze.png'
     with Image.open(img_path) as maze:
-        print(maze.histogram())
+        w, h = maze.size  # 641, 641
+        # Find the entrance and exit coordinates
+        for i in range(w):
+            if maze.getpixel((i, 0)) == (0, 0, 0, 255):
+                start = (i, 0)
+                print('Start: {}'.format(start))
+            if maze.getpixel((i, h - 1)) == (0, 0, 0, 255):
+                end = (i, h - 1)
+                print('Exit: {}'.format(end))
+
+        # Shortest path
 
     return 0
 
