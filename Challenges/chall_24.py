02b84e8a1c4094ce3f2d85c4908cd917ec4dbf04
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-15 08:28:37 -0500

    Solve challenge 24

--- Challenges/chall_24.py
+++ Challenges/chall_24.py
@@ -26,42 +26,43 @@ def main():
         # Find the entrance and exit coordinates
         for i in range(w):
             if maze.getpixel((i, 0)) == (0, 0, 0, 255):
-                start = (i, 0)
+                start = (i, 0)  # (630, 0)
                 print('Start: {}'.format(start))
             if maze.getpixel((i, h - 1)) == (0, 0, 0, 255):
-                end = (i, h - 1)
-                print('Exit: {}'.format(end))
+                end = (i, h - 1)  # (1, 640)
+                # print('Exit: {}'.format(end))
 
         # Breadth-first shortest path
-        shortest = {}
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]  # (dw, dh)
-        path_queue = [start]
+        path_queue = [[start]]
         while path_queue:
-            # Get and remove oldest pixel in queue
-            last_pixel = path_queue.pop(0)
+            # Get and remove oldest path in queue
+            tmp_path = path_queue.pop(0)
+            # print('Testing path: {}'.format(tmp_path))
+            last_pixel = tmp_path[-1]
             if last_pixel == end:
+                shortest = tmp_path
+                print('Found shortest path')
                 break
             for d in directions:
                 next_pixel = (last_pixel[0] + d[0], last_pixel[1] + d[1])
-                if ((next_pixel not in shortest) and
-                    0 <= next_pixel[0] < w and 0 <= next_pixel[1] < h and
-                    maze.getpixel(next_pixel) != (255, 255, 255, 255)):
-                    # Pixel isn't already in the path and it's not white
-                    shortest[last_pixel] = next_pixel
-                    path_queue.append(next_pixel)
+                if ((next_pixel not in tmp_path) and
+                        (0 <= next_pixel[0] < w) and
+                        (0 <= next_pixel[1] < h) and
+                        (maze.getpixel(next_pixel) != (255, 255, 255, 255))):
+                    # Pixel not already in tmp_path/is in grid/not white
+                    # print('Adding next_pixel: {}'.format(next_pixel))
+                    new_path = tmp_path + [next_pixel]
+                    path_queue.append(new_path)
 
         r_data = []
-        # all_data = []
         img = Image.new('RGB', (w, h), color=(255, 255, 255))
 
-        pixel = start
-        while pixel != end:
+        for pixel in shortest:
             r_data.append(maze.getpixel(pixel)[0])
             img.putpixel(pixel, maze.getpixel(pixel))
-            # all_data.append(maze.getpixel(pixel))
-            pixel = shortest[pixel]
 
-        # img.save('./maze_chall_24/path.jpg')
+        img.save('./maze_chall_24/path_TMP.jpg')
 
         with open('./maze_chall_24/maze_tmp.zip', 'wb') as result:
             result.write(bytes(r_data[1::2]))
