70f464de958ed2fb78ea86f884eb464f03026056
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-15 08:40:16 -0500

    Start challenge 25

---
+++ Challenges/chall_25.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python3
+# Python Challenge - 25
+# http://www.pythonchallenge.com/pc/hex/lake.html
+# Username: butter; Password: fly
+# Keyword:
+
+
+def main():
+    '''
+    Hint: imagine how they sound; picture of lake with puzzle pieces overlaid
+    can you see the waves; name of image is lake1.jpg
+    Download wave files 1-25: http://www.pythonchallenge.com/pc/hex/lake[N].wav
+    '''
+
+    pass
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
