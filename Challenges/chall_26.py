d22920c23e35ba2c04e14e4da329b872d94dbfbe
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-16 15:14:36 -0500

    Finish send email part

--- Challenges/chall_26.py
+++ Challenges/chall_26.py
@@ -2,10 +2,10 @@
 # Python Challenge - 26
 # http://www.pythonchallenge.com/pc/hex/decent.html
 # Username: butter; Password: fly
-# Keyword:
+# Keyword: speedboat
 
 import smtplib
-from email.mime.text import MIMEText
+from email.message import EmailMessage
 
 
 def main():
@@ -14,25 +14,44 @@ def main():
     Picture of monkeys, underneath: Hurry up, I'm missing the boat
     you've got his email
     '''
+    send_email = False
 
-    leo = 'leopold.moz@pythonchallenge.com'
-    hk = 'heather.kusmierz@gmail.com'
+    if send_email:
+        leo = 'leopold.moz@pythonchallenge.com'
+        hk = 'hkuzdev@gmail.com'
+        pw = input('Email password:')
+        textfile = './email_chall_26/msg.txt'
 
-    # Open a plain text file for reading.  For this example, assume that
-    # the text file contains only ASCII characters.
-    msg = MIMEText('sorry')
+        # Open a plain text file for reading.  For this example, assume that
+        # the text file contains only ASCII characters.
+        with open(textfile) as fp:
+            msg = EmailMessage()
+            msg.set_content(fp.read())
 
-    # me == the sender's email address
-    # you == the recipient's email address
-    msg['Subject'] = 'Apology'
-    msg['From'] = hk
-    msg['To'] = leo
+        msg['Subject'] = 'The contents of %s' % textfile
+        msg['From'] = hk
+        msg['To'] = leo
 
-    # Send the message via our own SMTP server, but don't include the
-    # envelope header.
-    s = smtplib.SMTP('localhost')
-    s.sendmail(hk, [leo], msg.as_string())
-    s.quit()
+        # Send the message via our own SMTP server, but don't include the
+        # envelope header.
+        s = smtplib.SMTP('smtp.gmail.com', '587')
+        s.ehlo()
+        s.starttls()
+        s.login(hk, pw)
+        # s.sendmail(hk, [leo], msg.as_string())
+        s.send_message(msg)
+        s.quit()
+
+    # Response
+    '''
+    Never mind that.
+
+    Have you found my broken zip?
+
+    md5: bbb8b499a0eef99b52c7f13f4e78c24b
+
+    Can you believe what one mistake can lead to?
+    '''
 
     return 0
 
