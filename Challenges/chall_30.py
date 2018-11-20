9a8969467cc7ae90607e6c2df4d761384247ac31
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-20 10:34:23 -0500

    Set up challenge 30

---
+++ Challenges/chall_30.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python3
+# Python Challenge - 30
+# http://www.pythonchallenge.com/pc/ring/yankeedoodle.html
+# http://www.pythonchallenge.com/pc/ring/yankeedoodle.csv
+# Username: repeat; Password: switch
+# Keyword:
+
+import csv
+
+
+def main():
+    '''
+    Hint: relax you are on 30; picture of beach scene
+    The picture is only meant to help you relax
+    while you look at the csv file
+    '''
+    return 0
+
+
+if __name__ == '__main__':
+    main()
