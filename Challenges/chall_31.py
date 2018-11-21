a913205242b6271b7050a40bf5e6d986aada9046
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-21 12:17:21 -0500

    Set up challenge 31

---
+++ Challenges/chall_31.py
@@ -0,0 +1,27 @@
+#!/Applications/anaconda/envs/imgPIL/bin
+# Python Challenge - 31
+# http://www.pythonchallenge.com/pc/ring/grandpa.html
+# http://www.pythonchallenge.com/pc/ring/yankeedoodle.csv
+# Username: repeat; Password: switch
+# Keyword:
+
+'''
+Uses Anaconda environment with Pillow for image processing
+    - Python 3.7, numpy, and Pillow (PIL)
+    - Run `source activate imgPIL`, `python chall_31.py`
+'''
+
+from PIL import Image
+
+
+def main():
+    '''
+    Hint: where am I? Picture of rock near a beach
+    short break, this ***REALLY*** has nothing to do with Python
+    Login required: island: country
+    '''
+    return 0
+
+
+if __name__ == '__main__':
+    main()
