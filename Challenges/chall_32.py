7d3f33ab6a3e604fa6512e7e90437cdb83fe1de1
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-11-24 11:32:55 -0500

    Set up challenge 32

---
+++ Challenges/chall_32.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3
+# Python Challenge - 32
+# http://www.pythonchallenge.com/pc/rock/arecibo.html
+# Username: kohsamui; Password: thailand
+# Keyword:
+
+'''
+Uses Anaconda environment with Pillow for image processing
+    - Python 3.7, numpy, and Pillow (PIL)
+    - Run `source activate imgPIL`, `python chall_32.py`
+'''
+
+from PIL import Image
+
+
+def main():
+    '''
+    Hint: etch-a-sketch
+    fill in the blanks
+    for warmup.txt
+    '''
+    return 0
+
+
+if __name__ == '__main__':
+    main()
