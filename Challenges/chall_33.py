c8db40b4f08d2e3bb0bf879ecd3b10ee9c06dc89
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-12-03 20:13:00 -0500

    Get image

--- Challenges/chall_33.py
+++ Challenges/chall_33.py
@@ -1,8 +1,11 @@
 #!/usr/local/bin/python3
 # Python Challenge - 33
 # http://www.pythonchallenge.com/pc/rock/beer.html
+# http://www.pythonchallenge.com/pc/rock/beer2.png
 # Username: kohsamui; Password: thailand
 # Keyword:
+
+from PIL import Image
 
 
 def main():
@@ -12,6 +15,8 @@ def main():
         remove its power, with its might.
         Then from the ashes, fair and square,
         another truth at you will glare.
+    Image is beer1.jpg -> beer2.jpg says "no, png"
+    New page is X on black and white
     '''
     return 0
 
