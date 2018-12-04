053a1368b023c101d411a935f7ab64f7cc8d10bd
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-12-03 20:09:26 -0500

    Set up challenge 33

---
+++ Challenges/chall_33.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python3
+# Python Challenge - 33
+# http://www.pythonchallenge.com/pc/rock/beer.html
+# Username: kohsamui; Password: thailand
+# Keyword:
+
+
+def main():
+    '''
+    Hint: 33 bottles of beer on the wall
+    If you are blinded by the light,
+        remove its power, with its might.
+        Then from the ashes, fair and square,
+        another truth at you will glare.
+    '''
+    return 0
+
+
+if __name__ == '__main__':
+    main()
